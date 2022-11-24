bb015a9a1a92279202aab3588f0b5cea745ff2fe
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-24 09:24:56 +0100

    Changes

--- Ejercicio01.java
+++ Ejercicio01.java
@@ -7,8 +7,14 @@ public class Ejercicio01{
         num[6] = 14;
         num[8] = 5;
         num[9] = 120;
+        System.out.print("Indice |");
         for(int i = 0; i < 12; i++){
-            System.out.printf("num[%2d]: %d\n",i,num[i]);
+            System.out.printf("%4d |",i);
+        }
+        System.out.print("\nValor  |");
+
+        for(int i = 0; i < 12; i++){
+            System.out.printf("%4d |",num[i]);
         }
     }
 }
