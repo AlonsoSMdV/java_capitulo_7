a04a0becc66d477cd9d58dc398fd2c876d2bf997
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-28 21:04:57 +0100

    New Commit

---
+++ EjercicioBidimensional01.java
@@ -0,0 +1,28 @@
+public class EjercicioBidimensional01 {
+    public static void main(String[] args){
+        int[][] num = new int[3][6];
+        num[0][0]  = 0;
+        num[0][1]  = 30;
+        num[0][2]  = 2;
+        num[0][5]  = 5;
+        num[1][0]  = 75;
+        num[1][4]  = 0;
+        num[2][2]  = -2;
+        num[2][3]  = 9;
+        num[2][5]  = 11;
+        int columna = 0;
+        int fila = 0;
+        System.out.print("         ");
+        for(columna = 0; columna < 6; columna++){
+            System.out.print("  Columna "+columna);
+        }
+        System.out.println();
+        for(fila = 0; fila < 3; fila++){
+            System.out.print("\nFila  "+fila);
+
+            for(columna = 0; columna < 6; columna++){
+                System.out.printf("%9d  ",num[fila][columna]);
+            }
+        }
+    }
+}
