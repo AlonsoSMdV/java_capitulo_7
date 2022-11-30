d0e7c1f767a96889900d100cae81547cd59f5812
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-30 18:41:43 +0100

    New Commit

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -4,7 +4,7 @@ public class Ejercicio17 {
         Scanner sc = new Scanner(System.in);
         int[] n = new int[10];
         for (int i = 0; i < 10; i++) {
-        n[i] = (int)(Math.random() * 381) + 20;
+        n[i] = (int)(Math.random() * 101) ;
         }
         System.out.println("\n\nArray original:");
 
